8525823a067ba079edded4f68f958cab686a888c
Charlie Wu <wujibang@gmail.com>
2017-10-06 23:55:31 -0400

    https support with ngrok

--- amazon_review/amazon_review/settings.py
+++ amazon_review/amazon_review/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q4uyn)zwjimw)-#v8+_kgew*g04rkgb-x!mjj#ym&mj!lv%2=$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
