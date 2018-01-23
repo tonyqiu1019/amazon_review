4eaf0cf484b100af748193d89f6ef4ae3254cac4
Jerry Sun <jerrysun@d-172-25-209-65.dhcp.virginia.edu>
2018-01-23 14:48:08 -0500

    highlight deprecated

--- amazon_review/api/urls.py
+++ amazon_review/api/urls.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from . import views
 urlpatterns = [url(r'^$', views.index),
                 url(r'^amazon$', views.prod),
-                url(r'^review$', views.highlight)]
+                # url(r'^review$', views.highlight)
+                ]
