62947c04ae256bc46b205f412ab0a32927d5c121
Jerry Sun <jerrysun@d-172-25-203-68.dhcp.virginia.edu>
2018-01-25 15:48:46 -0500

    Revert "highlight deprecated"
    This reverts commit 4eaf0cf484b100af748193d89f6ef4ae3254cac4.

--- amazon_review/api/views.py
+++ amazon_review/api/views.py
@@ -16,41 +16,44 @@ def _success(stat, data):
 def _fail(stat, error_msg):
     return JsonResponse({ 'stat': stat, 'error': error_msg })
 
+def index(request):
+    return HttpResponse("Hello World!")
+
 def prod(request):
     query = request.GET.dict()
     asin = query['asin']
     # prod, properties, reviews = parse(asin)
     # parse.delay(asin)
-    api_entry(asin)
+    parse(asin)
     # relationships = match.keyword_match(properties, reviews, prod)
     # save_relationship(relationships, prod)
     ret = find_relationship(asin)
     return _success(200, ret)
 
-def api_entry(asin):
-    prod_query = parser.ParseProduct(asin)
-    parsed, prod, properties = save_prod(prod_query)
-    if parsed:
-        parse(asin, prod, properties)
-        return
-    else :
-        first_time_parse(asin, prod, properties)
-        return
-    return
+def highlight(request):
+    query = request.GET.dict()
+    review_id = query['review']
+    related_review = Review.objects.get(pk=review_id)
+    asin = related_review.prod.asin
+    related_prod = Product.objects.get(pk=asin)
+    all_relation = Relationship.objects.filter(related_review=related_review, prod=related_prod)
+    ret_list = []
+    for relation in all_relation:
+        ret_list.append({'sentence':relation.best_sentence,
+        'property' : relation.related_property.topic,
+        'sentiment' : relation.sentiment})
+    return _success(200, {'content':ret_list})
 
-@shared_task
-def parse(asin, prod, properties):
+# @shared_task
+def parse(asin):
     reviews = parser.ParseReviews(asin)
     # print(reviews)
+    prod_query = parser.ParseProduct(asin)
+    prod, properties = save_prod(prod_query)
     saved_reviews = save_review(reviews, prod)
     # print(saved_reviews)
     ret = match.keyword_match(properties, saved_reviews, prod)
     # print(len(ret))
-    return
-
-def first_time_parse(asin, prod, properties):
-    per_item_parser(asin, prod, properties)
-    parse(asin, prod, properties)
     return
 
 def find_relationship(prod):
@@ -67,16 +70,14 @@ def find_relationship(prod):
 def save_prod(query):
     asin = query['asin']
     product_name = query['name']
-    parsed = False
     try:
         prod = Product.objects.get(pk=asin)
         properties = Property.objects.filter(prod=prod)
-        parsed = True
     except ObjectDoesNotExist:
         prod = Product.objects.create(asin=asin, title=product_name)
         prod.save()
         properties = save_property(query, prod)
-    return parsed, prod, properties
+    return prod, properties
 
 def save_review(reviews, prod):
     saved_reviews = []
@@ -97,19 +98,3 @@ def save_property(query, prod):
         property.save()
         saved_properties.append(property)
     return saved_properties
-
-#-----deprecated-----#
-
-# def highlight(request):
-#     query = request.GET.dict()
-#     review_id = query['review']
-#     related_review = Review.objects.get(pk=review_id)
-#     asin = related_review.prod.asin
-#     related_prod = Product.objects.get(pk=asin)
-#     all_relation = Relationship.objects.filter(related_review=related_review, prod=related_prod)
-#     ret_list = []
-#     for relation in all_relation:
-#         ret_list.append({'sentence':relation.best_sentence,
-#         'property' : relation.related_property.topic,
-#         'sentiment' : relation.sentiment})
-#     return _success(200, {'content':ret_list})
