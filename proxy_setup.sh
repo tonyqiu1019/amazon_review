85f5e548fea896df8671fda39b01322b10241129
Charlie Wu <wujibang@gmail.com>
2018-02-15 13:39:21 -0500

    proxy setup script

---
+++ proxy_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ssh -D 8123 -f -C -q -N jw7jb@hcdm1.cs.virginia.edu
+ssh -D 8124 -f -C -q -N jw7jb@hcdm2.cs.virginia.edu
+ssh -D 8125 -f -C -q -N jw7jb@hcdm3.cs.virginia.edu
+ssh -D 8126 -f -C -q -N jw7jb@hcdm4.cs.virginia.edu
+ssh -D 8127 -f -C -q -N jw7jb@hcdm5.cs.virginia.edu
