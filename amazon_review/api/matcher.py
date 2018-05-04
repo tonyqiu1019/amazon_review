534a19404796c2ab6c9d8e72195f563502b8743a
tonyqiu1019 <tq7bw@virginia.edu>
2018-05-04 17:19:17 -0400

    htmm matcher function

--- amazon_review/api/matcher.py
+++ amazon_review/api/matcher.py
@@ -7,15 +7,29 @@ from nltk import tokenize
 from nltk.tokenize.treebank import TreebankWordTokenizer
 from nltk.stem.snowball import SnowballStemmer
 
-from .topWords import topics
+from pyhtmm.utils import load_pickle
+from pyhtmm.process import process_doc
+
+sid = SentimentIntensityAnalyzer()
+htmm = load_pickle('./htmm_trained_model.pickle')
+
+idx_topic = [
+    "Screen Size",
+    "Graphics Coprocessor",
+    "Processor",
+    "RAM",
+    "Operating System",
+    "Hard Drive",
+    "Number of USB 3.0 Ports",
+    "Average Battery Life (in hours)",
+]
 
 def keyword_match(properties, reviews):
-    sid = SentimentIntensityAnalyzer()
     ret = []
 
     for p in properties:
-        text = p["text_content"];
-        property_word = [" " + word.lower() + " " for word in text.split(" ")]
+        text = p["text_content"]
+        p_word = [" " + word.lower() + " " for word in text.split(" ")]
         for review in reviews:
             if not isinstance(review["content"], str): continue
             sentences = tokenize.sent_tokenize(review["content"])
@@ -23,15 +37,42 @@ def keyword_match(properties, reviews):
             maxCount = 0
             best_sentence = ''
             for i in range(len(sentences)):
-                count = sum(1 for word in property_word if word in sentences[i].lower())
+                count = sum(1 for w in p_word if w in sentences[i].lower())
                 if count > maxCount:
                     maxCount = count
                     best_sentence = sentences[i]
             if maxCount >= 5:
                 ps = sid.polarity_scores(best_sentence.lower())['compound']
-                ret.append({'related_property_id': p["id"], 'best_sentence': best_sentence, 'related_review_id': review["id"], 'sentiment': ps})
+                ret.append({
+                    'related_property_id': p["id"],
+                    'best_sentence': best_sentence,
+                    'related_review_id': review["id"],
+                    'sentiment': ps,
+                })
 
+    find_cluster(ret)
     return ret
 
 def htmm_inference(properties, reviews):
-    pass
+    ret = []
+
+    idx_id = [""] * len(idx_topic)
+    for p in properties:
+        for i in range(len(idx_topic)):
+            if idx_topic[i] == p["topic"]: idx_id[i] = p["id"]
+
+    for review in reviews:
+        doc = process_doc(review["content"])
+        path, entropy = htmm.predict_topic(doc)
+        for i, stn in enumerate(doc.sentence_list):
+            if entropy[i] >= 0.1: continue
+            ps = sid.polarity_scores(stn.raw_content.lower())['compound']
+            ret.append({
+                'related_property_id': idx_id[path[i] % 8],
+                'best_sentence': stn.raw_content,
+                'related_review_id': review["id"],
+                'sentiment': ps,
+            })
+
+    find_cluster(ret)
+    return ret
